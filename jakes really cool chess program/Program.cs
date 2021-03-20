7c78b12d2188ea3c5d62871d35574dc9e29fc5ab
RealJeffBezos <48831476+jimber404@users.noreply.github.com>
2021-03-20 16:13:43 +0000

    first push
    just the bare bones of the program

---
+++ jakes really cool chess program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace jakes_really_cool_chess_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player myObj = new Player("JakeI", 1500);
+        }
+    }
+
+    public class Player
+    {
+        private int gamesWon;
+        private int gamesLost;
+        private int elo;
+        public string name;
+        public Player(string nameIn,int eloIN)
+        {
+            name = nameIn;
+            elo = eloIN;
+        }
+
+        public int Elo
+        { get; set; }
+
+        public double ExpectationToWin(int playerTwoRating)
+        {
+            return 1 / (1 + Math.Pow(10, (playerTwoRating - elo) / 400.0));
+        }
+    }
+}
